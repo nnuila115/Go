846bc944048d75d33e1871bcbf63ac16ab1f50af
Nic <nanuila115>
2025-06-23 22:51:36 -0400

    types and whatnot

---
+++ tutorials/cmd/fireship/types.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+	const a int32 = 12
+	const b float32 = 20.5
+	var c complex128=1+4i
+	var d uint16=14
+
+	n := 42
+	pi := 3.14
+	x,y :=true, false
+	z := "this go operator really is something huh"
+
+	fmt.Printf("user-specified types:\n %T %T %T %T\n", a, b, c, d)	
+	fmt.Printf("default types:\n %T %T %T %T %T\n", n, pi, x, y, z)
+}
